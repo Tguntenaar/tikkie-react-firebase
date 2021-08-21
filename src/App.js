81a8077b7367dd47958a2bbe9f1c7d6d6d31f281
Joost Guntenaar <joost@iMac27.local>
2021-08-21 19:39:23 +0200

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCMaAgcsxMLVBKw436cd_K91XQR4xHcIP0",
+    authDomain: "tikkie-react.firebaseapp.com",
+    projectId: "tikkie-react",
+    storageBucket: "tikkie-react.appspot.com",
+    messagingSenderId: "283304463236",
+    appId: "1:283304463236:web:9eec5a6a81772b1ba8abd1",
+    measurementId: "G-K4HGGKM3MM"
+  };
 
 function App() {
   return (
@@ -7,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yes
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
